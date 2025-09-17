c4f499587b32a74c3f12b569755c09c4be6ce33e
ALBATEQ <dtecnico@albateq.com>
2025-09-16 19:18:42 -0500

    Update app_prediccion.py

--- app_prediccion.py
+++ app_prediccion.py
@@ -10,7 +10,7 @@ import os
 st.markdown(
     """
     <div style='display: flex; justify-content: center; margin-bottom: -60px; margin-top: 10px;'>
-        <img src='logo_mejorado.jpg' width='180' style='opacity:0.15; filter: grayscale(50%);'/>
+        <img src='LOGO_ALBATEQ.svg' width='180' style='opacity:0.15; filter: grayscale(50%);'/>
     </div>
     """,
     unsafe_allow_html=True
@@ -92,6 +92,7 @@ if st.button('Generar Informe'):
     ax2.set_title('Peso estimado vs real por Cons Acumulado')
     ax2.legend()
     st.pyplot(fig2)
+
 
 
 
